580989b81b8f4c6d10b48d407214bb73874a16ca
Alex Strelets <alex.strelets@gmail.com>
2024-12-13 15:58:28 +0200

    CardsCarousel component and integrate with MapRoot, enhance drawer functionality

---
+++ src/components/Map/CardsCarousel.tsx
@@ -0,0 +1,60 @@
+import { Box } from '@mui/material'
+
+import { useSearch } from 'providers/SearchProvider'
+
+export const CDN = 'https://cdn.repliers.io'
+
+export const getCDNPath = (fileName: string, size = 'large') => {
+  return `${CDN}/${fileName}?&webp&class=${size}`
+}
+
+const CardsCarousel = ({ drawer }: { drawer: boolean }) => {
+  const { listings } = useSearch()
+  return (
+    <Box
+      sx={{
+        display: drawer ? 'flex' : 'none', // Updated to 'flex'
+        flexDirection: 'row',
+        boxSizing: 'border-box',
+        position: 'absolute',
+        scrollbarWidth: 'thin',
+        overflowY: 'hidden',
+        overflowX: 'auto',
+        bottom: -12,
+        left: 16,
+        right: 16,
+        pb: 3.5
+      }}
+    >
+      {listings.map((listing, i) => (
+        <Box
+          key={i}
+          sx={{
+            mr: 1.25,
+            '&:last-child': { mr: 0 },
+            flex: '0 0 auto',
+            width: 200,
+            bgcolor: '#FFF',
+            borderRadius: 2,
+            boxShadow: 1,
+            p: 1.25
+          }}
+        >
+          <Box
+            sx={{
+              borderRadius: 1,
+              bgcolor: 'text.hint',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              backgroundImage: `url(${getCDNPath(listing.images[0], 'small')})`,
+              height: 66,
+              width: 100
+            }}
+          />
+        </Box>
+      ))}
+    </Box>
+  )
+}
+
+export default CardsCarousel
