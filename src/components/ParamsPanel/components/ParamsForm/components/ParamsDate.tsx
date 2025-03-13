ecead17d0a54ec50ad957d34552c5780de8f4a1b
Alex Strelets <alex.strelets@gmail.com>
2025-03-13 16:38:38 +0200

    cleanup

--- src/components/ParamsPanel/components/ParamsForm/components/ParamsDate.tsx
+++ src/components/ParamsPanel/components/ParamsForm/components/ParamsDate.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
 import { useFormContext } from 'react-hook-form'
 
