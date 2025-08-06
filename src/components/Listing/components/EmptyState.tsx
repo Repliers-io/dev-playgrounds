52410d87e44b5c4af60356eb9de5d3e21b058f67
K. Sokolinskyi <kostiantyn@srost-studio.com>
2025-08-06 12:31:16 -0400

    fix: update empty state messaging in Listings component

--- src/components/Listing/components/EmptyState.tsx
+++ src/components/Listing/components/EmptyState.tsx
@@ -1,3 +1,4 @@
+import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward'
 import { Box, Typography } from '@mui/material'
 
 const EmptyState = () => {
@@ -14,11 +15,25 @@ const EmptyState = () => {
       }}
     >
       <Typography variant="h6" color="text.secondary" gutterBottom>
-        No Property Data
+        No Listing Data
       </Typography>
       <Typography variant="body2" color="text.secondary">
-        Enter an mlsNumber AND a boardId in the Property Parameters to view
-        property details.
+        Enter an <i>mlsNumber</i> in the Listing Parameters to view listing
+        details.
+        <br />
+        <br />
+        <i>boardId</i> will be needed for API keys having access to{' '}
+        <a
+          target="_blank"
+          href="https://help.repliers.com/en/article/understanding-and-using-boardids-in-the-repliers-api-lfywn2/"
+          style={{
+            color: 'inherit',
+            textDecoration: 'none',
+            gap: '2px'
+          }}
+        >
+          multiple boards <ArrowOutwardIcon sx={{ my: -0.5, fontSize: 16 }} />
+        </a>
       </Typography>
     </Box>
   )
