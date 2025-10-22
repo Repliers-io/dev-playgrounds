d21be19e4575a35db698e1a1437a477e7bee4d45
Alex Strelets <alex.strelets@gmail.com>
2025-10-23 02:25:37 +0300

    colors

--- src/components/Chat/constants.ts
+++ src/components/Chat/constants.ts
@@ -1,5 +1,5 @@
 import { lighten } from '@mui/material'
 
-export const aiColor = '#FFCB63'
-export const aiBgColor = lighten(aiColor, 0.8)
-export const clientBgColor = lighten('#CBFF63', 0.8)
+// export const aiColor = '#FFCB63'
+export const aiBgColor = lighten('#98a4f2', 0.6)
+export const clientBgColor = lighten('#dec', 0.2)
